0c6592546aa57e79221eab98714ab918af15532f
aocarmo <alex.carmo91@gmail.com>
2019-07-07 17:08:02 -0300

    Inicializando

---
+++ config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  2 16:38:20 2019
+
+@author: alex
+"""
+
+PATH_CONFIG = {
+    'webDrivePathChrome': r'C:\Projetos\Python\TvAratuAutomacaoRealTime\driver\chromedriver.exe',
+    'webDrivePathFireFox': r'C:/Projetos/Python/TvAratuAutomacaoRealTime/driver/geckodriver.exe',
+    'resourcesPath': 'C:\Projetos\Python\TvAratuAutomacaoRealTime\Resources\\',
+    'pathRelatorio': 'C:\Projetos\Python\TvAratuAutomacaoRealTime\\',
+}
+
+
+REAL_TIME_CONFIG = {
+    'user': 'RENATA_ESQUIVEL',
+    'password': 'Minuto##19',
+    'url': 'https://www.realtimebrasil.com/',
+   
+}
